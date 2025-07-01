4172f99f51b9ded1183896d946788bc22eead3d8
Shivpanda <gamenian098@gmail.com>
2025-07-01 21:22:51 +0530

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+ int main() {
+     int dist;
+     int time;
+     std::cout << "enter the distance" << std::endl;
+     std::cout << "enter the time " << std::endl;
+     std::cin >> dist >> time ;
+     std::cout << " motherfucker you are going 0" << dist/time << "m/s" <<std::endl;
+     return 0;
+ }
